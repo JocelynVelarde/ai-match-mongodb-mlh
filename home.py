1ba9c7e9a60ed66286d1c26a46a8e7afe22d344b
JocelynVelarde <a01285780@itesm.mx>
2024-12-08 20:03:15 -0600

    added button to handle responses

--- home.py
+++ home.py
@@ -13,3 +13,17 @@ st.divider()
 
 # Add a subheader
 st.subheader("Fill the form to get matched")
+
+st.text_input("What is your name")
+st.text_input("What are your programming languages")
+st.text_input("What is your favorite framework")
+st.text_input("What is your favorite tech conference or blog post")
+st.text_input("Have you contributed to open source, name projects")
+st.text_input("What is your favorite tech celebrity")
+st.text_input("Favorite code editor")
+
+if st.button("This is a button"):
+    # Generate potential matches for my tech form
+    st.success("Yai you have been matched!")
+else:
+    st.warning("Something went wrong")
