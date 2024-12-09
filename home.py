2b75575020abcb5ba75a33a9fa238ccf5e41811f
JocelynVelarde <a01285780@itesm.mx>
2024-12-08 19:50:44 -0600

    ui changes on home

---
+++ home.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+# Write a title
+st.title("Matching app for techies")
+
+# Place a divider
+st.divider()
+
+# Write a text
+st.write("Welcome to my app fill the form to get started")
+
+st.divider()
+
+# Add a subheader
+st.subheader("Fill the form to get matched")
